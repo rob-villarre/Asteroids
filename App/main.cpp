c9dc76e1719fbab763809ffb66f092844f416b50
Roberto Villarreal Andrade <rob.villarre@gmail.com>
2025-11-29 13:42:48 -0700

    initial commit

---
+++ App/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../Juno/Include/library.h"
+
+int main()
+{
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++)
+    {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    hello();
+
+    return 0;
+}
